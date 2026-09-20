3300de2191273f3d1923c65e1c0c0a8eb54aae6f
4wy2 <69701487+4wy2@users.noreply.github.com>
2026-09-20 03:28:15 +0300

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,7 @@
+import MoneyApp from './money-app';
+import { getChatGPTUser } from './chatgpt-auth';
+export const dynamic = 'force-dynamic';
+export default async function Home() {
+  const user = await getChatGPTUser();
+  return <MoneyApp signedIn={!!user} />;
+}
